95f690d18300224885489c7d1a9852beff24e0e5
dvs2000 <dorivaldodossantos2000@gmail.com>
2023-08-23 12:07:58 +0100

    Config fonts and imgs files

--- lib/src/views/utils/consts_utils.dart
+++ lib/src/views/utils/consts_utils.dart
@@ -1,8 +1,19 @@
+import 'dart:ui';
+
 class ConstsUtils {
   
   // FONTS
+  static const fontItalic     = "italic";
+  static const fontMedium     = "medium";
+  static const fontRegular    = "regular";
+  static const fontSemiBold   = "semi-bold";
+  static const fontBold       = "bold";
 
   // COLORS
+  static const colorPrimary   = Color(0xff00B4CC);
 
   // IMAGES
+  static const imgCondicionador = "assets/imgs/condicionador.png";
+  static const imgMascara       = "assets/imgs/mascara.png";
+  static const imgSearch        = "assets/imgs/search.png";
 }
